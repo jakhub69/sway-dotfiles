c9560fe604394de38fe88e47eda22fa0bc9dfc42
jakhub <jakhub@proton.me>
2025-12-18 18:36:11 +0100

    fix sysmenu and config

--- sway/scripts/sysmenu.sh
+++ sway/scripts/sysmenu.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
-menu_cmd="$@"
+BEMENU_OPTS="-i --fb #303446 --ff #c6d0f5 --nb #303446 --nf #c6d0f5 --tb #303446 --hb #303446 --tf #e78284 --hf #e5c890 --af #c6d0f5 --ab #303446 --fn 'CommitMono 13'"
+OPTIONS="󰌾 Lock\n󰗼 Logout\n󰜉 Reboot\n󰐥 Poweroff\n󰤄 Suspend"
+ACTION=$(printf "$OPTIONS" | bemenu $BEMENU_OPTS -p "System:")
 
-options="Lock\nPoweroff\nReboot\nSuspend\nLogout"
+[ -z "$ACTION" ] && exit
 
-action=$(echo "$options" | $menu_cmd -p "Select action:")
+CONFIRM=$(printf "No\nYes" | bemenu $BEMENU_OPTS -p "Are you sure you want to: $ACTION?")
 
-[ -z "$action" ] && exit
-
-confirm=$(echo "Yes\nNo" | $menu_cmd -p "Confirm $action?")
-
-if [ "$confirm" = "Yes" ]; then
-    case "$action" in
-        Lock) swaylock -f ;;
-        Poweroff) systemctl poweroff ;;
-        Reboot) systemctl reboot ;;
-        Suspend) systemctl suspend ;;
-        Logout) swaymsg exit ;;
+if [ "$CONFIRM" = "Yes" ]; then
+    case "$ACTION" in
+        *Lock)     swaylock -f ;;
+        *Logout)   swaymsg exit ;;
+        *Reboot)   systemctl reboot ;;
+        *Poweroff) systemctl poweroff ;;
+        *Suspend)  systemctl suspend ;;
     esac
 fi
