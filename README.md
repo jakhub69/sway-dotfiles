d34441bfa825b3cc41850b6890a2f02315b0425b
jakhub <jakhub@proton.me>
2025-12-18 18:19:41 +0100

    add dotfiles

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# sway-dotfiles
-Dotfiles for Sway (Fedora 43)
+# Fedora Sway Dotfiles
+
+![Sway](https://i.imgur.com/KZXqSby.jpeg)
+
+## Tutorial
+
+```
+sudo dnf install sway alacritty xdg-desktop-portal-wlr swaybg swaylock swayidle bemenu mako wl-clipboard grim slurp libnotify unzip ripgrep imv  j4-dmenu-desktop
+curl -sLO https://github.com/eigilnikolajsen/commit-mono/releases/download/v1.143/CommitMono-1.143.zip
+unzip CommitMono-1.143.zip 
+mkdir -p "$HOME/.local/share/fonts"
+cp CommitMono*/*.otf "$HOME/.local/share/fonts/"
+rm -r CommitMono*
+curl -sLO https://github.com/ryanoasis/nerd-fonts/releases/latest/download/NerdFontsSymbolsOnly.zip
+unzip NerdFontsSymbolsOnly.zip -d NerdFonts/
+cp NerdFonts/SymbolsNerdFont* .local/share/fonts/
+rm -r NerdFonts*
+fc-cache -f -v >/dev/null
+sudo wget https://raw.githubusercontent.com/nwg-piotr/autotiling/refs/heads/master/autotiling/main.py /usr/local/bin/autotiling
+chmod +x /usr/local/bin/autotiling
+```
---
+++ sway/scripts/statusbar.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# ---------------- CPU Usage (Dynamic & Rounded) ----------------
+cpu=$(grep 'cpu ' /proc/stat)
+idle=$(echo $cpu | awk '{print $5+$6}')
+total=$(echo $cpu | awk '{print $2+$3+$4+$5+$6+$7+$8}')
+
+sleep 0.2
+
+cpu_new=$(grep 'cpu ' /proc/stat)
+idle_new=$(echo $cpu_new | awk '{print $5+$6}')
+total_new=$(echo $cpu_new | awk '{print $2+$3+$4+$5+$6+$7+$8}')
+
+cpu_usage=$(echo "$idle $total $idle_new $total_new" | awk '{print int(100 * (1 - ($3 - $1) / ($4 - $2)) + 0.5)}' )
+cpu_display="󰍛 $cpu_usage%"
+
+# ---------------- RAM Usage ----------------
+ram=$(free -h | awk '/^Mem:/ {print "󰘚 " $3 "/" $2}')
+
+# ---------------- Network ----------------
+if ip link show | grep -E "state UP" | grep -qE "eth|enp|eno"; then
+    net_icon="󰈀"
+    network="$net_icon Wired"
+else
+    wifi=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d: -f2)
+    if [ -n "$wifi" ]; then
+        network="󰖩 $wifi"
+    else
+        network="󰖪 No Net"
+    fi
+fi
+
+# ---------------- Volume ----------------
+vol_line=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+vol_val=$(echo "$vol_line" | awk '{printf "%d\n", $2 * 100}')
+if echo "$vol_line" | grep -q "\[MUTED\]"; then
+    vol_icon="󰖁"
+else
+    vol_icon="󰖀"
+fi
+volume="$vol_icon $vol_val%"
+
+# ---------------- Clock ----------------
+clock="$(date '+󰥔 %H:%M 󰃭 %d.%m.%Y')"
+
+# ---------------- Combine ----------------
+echo "$cpu_display | $ram | $network | $volume | $clock"
